72c9cd07a787b9589761e599ad7b6be4220ef1f4
Merrill Gutzwiller <mbgutzwiller@protonmail.ch>
2025-04-07 18:58:32 +0200

    save

--- examples/led/convergence.py
+++ examples/led/convergence.py
@@ -22,7 +22,7 @@ if __name__ == "__main__":
 
         velocity_set = xlb.velocity_set.D2Q4(precision_policy=precision_policy, compute_backend=compute_backend)
 
-        grid_sizes = [10, 20, 40, 80, 120, 160, 240, 320]
+        grid_sizes = [10, 20, 40, 80, 120, 160, 240, 320, 640]
         num_stepss = [int(grid_size * 2.5) for grid_size in grid_sizes]
 
         l2_errors_u = []
