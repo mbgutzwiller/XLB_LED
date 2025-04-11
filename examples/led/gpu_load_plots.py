5e99b8dedb1f889d07c9baddcefb563c9f9fe60a
Merrill Gutzwiller <mbgutzwiller@protonmail.ch>
2025-04-11 20:21:18 +0200

    save

--- examples/led/gpu_load_plots.py
+++ examples/led/gpu_load_plots.py
@@ -9,11 +9,18 @@ script_dir = os.path.dirname(os.path.abspath(__file__))
 figures_dir = os.path.join(script_dir, "figures_final_scaling")
 os.makedirs(figures_dir, exist_ok=True)
 
-grid_sizes = np.array([50, 100, 200, 300, 400, 600, 800, 1600, 2400, 3200, 4000])
-gpu_compute_util = np.array([20, 23, 48, 100, 100, 100, 100, 100, 100, 100, 100])
-gpu_mem_util = np.array([0, 0, 25, 67, 72, 73, 78, 78, 78, 79, 78])
-gpu_power = np.array([43.892, 45.393, 55.74, 74.692, 76.398, 78.013, 80.601, 80.626, 82.489, 81.335, 81.265])
-gpu_vram = np.array([492.5, 492.5, 492.5, 524.5, 556.5, 620.5, 748.5, 1100.5, 1740.5, 2668.5, 3724.5])
+grid_sizes = np.array([50, 100, 200, 400, 600, 800, 1600, 2400, 3200, 4000])
+# This is f0bound
+# gpu_compute_util = np.array([20, 23, 48, 100, 100, 100, 100, 100, 100, 100, 100])
+# gpu_mem_util = np.array([0, 0, 25, 67, 72, 73, 78, 78, 78, 79, 78])
+# gpu_power = np.array([43.892, 45.393, 55.74, 74.692, 76.398, 78.013, 80.601, 80.626, 82.489, 81.335, 81.265])
+# gpu_vram = np.array([492.5, 492.5, 492.5, 524.5, 556.5, 620.5, 748.5, 1100.5, 1740.5, 2668.5, 3724.5])
+
+# this is pulse
+gpu_compute_util = np.array([12, 14, 33, 100, 100, 100, 100, 100, 100])
+gpu_mem_util = np.array( [0, 0, 23, 91, 97, 100, 100, 100, 99])
+gpu_vram = np.array([492.5, 492.5, 492.5, 524.5, 716.5, 1100.5, 1740.5, 2572.5, 3788.5])
+gpu_power = np.array( [45.336, 46.417, 54.346, 80.225, 84.007, 87.032, 88.212, 88.737, 87.631])
 
 max_compute = 100./100.
 max_mem_util = 100./100.
